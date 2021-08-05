65cec88d819552f69a080cfbd0eaab72871fedc8
TatsianaKedrova <Baletrot@gmail.com>
2021-08-05 15:56:16 +0200

    created App2 as a class component.

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,35 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//example class component
+class App2 extends React.Component {
+
+        state = {
+            counter: 0
+        }
+
+    incrementCounter = () => {
+        // this.setState({counter: this.state.counter + 1})
+        this.setState( (prevState, props) => {
+            return {
+                counter: this.state.counter + 1
+            }
+        })
+    }
+    render() {
+        console.log("render")
+        return (
+            <div>
+                {this.state.counter}
+                <button onClick={this.incrementCounter}>Add</button>
+            </div>
+        )
+    }
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
